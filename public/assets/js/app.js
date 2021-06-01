ec35670e7b1e18c4dcc9c12e67eecc93abd24fb9
leoseq <leo.sequeira@hotmail.com>
2021-06-01 00:39:20 -0300

    Boton Llamar JS
    - Se agrega funcionalidad JS al boton de Llamar
    - Pendiente dar estilo al contenido dentro del boton

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,16 @@
 class appClinica {
-
 	constructor() {
-
+        //Inicializa la funcionalidad Menu (Test)
+    /*    document.addEventListener("DOMContentLoaded", () => {
+			Clinica.cargarScript("Menu", "assets/js/components/menu.js", () => {	
+				let menu = new Menu("nav");
+			});
+		});
+	*/
 		//Inicializa la funcionalidad Boton Llamar
         document.addEventListener("DOMContentLoaded", () => {
 			Clinica.cargarScript("BotonLlamar", "assets/js/components/botonLlamar.js", () => {	
-				let botonLlamar = new BotonLlamar("");
+				let botonLlamar = new BotonLlamar("body > main > button");
 			});
 		});
 		

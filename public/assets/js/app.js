96e80eb218fb0ea335a2b3414c1f7f96f9ceffcf
leoseq <leo.sequeira@hotmail.com>
2021-06-05 18:17:45 -0300

    Fix Boton Llamar
    - Fix Estilos

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,14 +1,14 @@
 class appClinica {
 	constructor() {
         
-		//Inicializa la funcionalidad Boton Llamar
+		// Inicializa la funcionalidad Boton Llamar
         document.addEventListener("DOMContentLoaded", () => {
 			Clinica.cargarScript("BotonLlamar", "assets/js/components/botonLlamar.js", () => {	
 				let botonLlamar = new BotonLlamar("#botonCall");		
 			});
 		});
 		
-		//Inicializa la funcionalidad del Drag And Drop
+		// Inicializa la funcionalidad del Drag And Drop
 		document.addEventListener("DOMContentLoaded", () => {
 			Clinica.cargarScript("DragAndDrop", "assets/js/components/dragAndDrop.js", () => {	
 				let dragAndDrop = new DragAndDrop(".inputDrop");		
--- src/App/views/parts/footer.view.php
+++ src/App/views/parts/footer.view.php
@@ -1,30 +1,3 @@
-<!--button id="botonCall">
-    <span class="telefono-icon-28"></span>
-    
-    <article>
-        <h3 style="text-align: center">Teléfonos</h3>
-
-        <a href="tel:+2364537977">
-            <span class="telefono-icon-28"></span>
-            11 1234-5678
-        </a>
-        <p>Atención telefónica las 24hs</p>
-
-        <a href="tel:+2364537977">
-            <span class="telefono-icon-28"></span>
-            11 1234-5678
-        </a>
-        <p>Lunes a viernes de 8:00 a 20:00 Hs</p>
-
-        <a href="tel:+2364537977">
-            <span class="telefono-icon-28"></span>
-            11 1234-5678
-        </a>
-        <p>Urgencias</p>
-    </article>
-</button-->
-
-
 <nav>
     <a href="https://www.facebook.com/" style="background: #4f2c7f">
         <span class="facebook-icon-24"></span>
